342fc1ae4d2aea280d46bb2b138ae357eccf99b5
dsausen <53322492+dsausen@users.noreply.github.com>
2019-09-01 22:27:34 -0300

    Final touches

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import io from 'socket.io-client';
 import {
   View,
   SafeAreaView,
@@ -12,10 +13,12 @@ import api from '../services/api';
 import logo from '../assets/logo.png';
 import like from '../assets/like.png';
 import dislike from '../assets/dislike.png';
+import itsAMatch from '../assets/itsamatch.png';
 
 export default function Main({navigation}) {
   const id = navigation.getParam('user');
   const [users, setUsers] = useState([]);
+  const [matchDev, setMatchDev] = useState(null);
 
   useEffect(() => {
     async function loadUsers() {
@@ -25,6 +28,16 @@ export default function Main({navigation}) {
       setUsers(response.data);
     }
     loadUsers();
+  }, [id]);
+
+  useEffect(() => {
+    const socket = io('http://192.168.0.13:3333', {
+      query: {user: id},
+    });
+
+    socket.on('match', dev => {
+      setMatchDev(dev);
+    });
   }, [id]);
 
   async function handleLike() {
@@ -88,6 +101,24 @@ export default function Main({navigation}) {
           </TouchableOpacity>
           <TouchableOpacity style={styles.button} onPress={handleLike}>
             <Image source={like} />
+          </TouchableOpacity>
+        </View>
+      )}
+      {matchDev && (
+        <View style={styles.matchContainer}>
+          <Image style={styles.matchImage} source={itsAMatch} />
+          <Image
+            style={styles.matchAvatar}
+            source={{
+              uri: matchDev.avatar,
+            }}
+          />
+
+          <Text style={styles.matchName}>{matchDev.name}</Text>
+          <Text style={styles.matchBio}>{matchDev.bio}</Text>
+
+          <TouchableOpacity onPress={() => setMatchDev(null)}>
+            <Text style={styles.closeMatch}>FECHAR</Text>
           </TouchableOpacity>
         </View>
       )}
@@ -169,5 +200,44 @@ const styles = StyleSheet.create({
       width: 0,
       height: 2,
     },
+  },
+  matchContainer: {
+    ...StyleSheet.absoluteFillObject,
+    zIndex: 5,
+    backgroundColor: 'rgba(0, 0, 0, 0.8)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  matchImage: {
+    height: 60,
+    resizeMode: 'contain',
+  },
+  matchAvatar: {
+    width: 160,
+    height: 160,
+    borderRadius: 80,
+    borderWidth: 5,
+    borderColor: '#fff',
+    marginVertical: 30,
+  },
+  matchName: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  matchBio: {
+    marginTop: 10,
+    fontSize: 16,
+    color: 'rgba(255, 255, 255, 0.8)',
+    lineHeight: 24,
+    textAlign: 'center',
+    paddingHorizontal: 30,
+  },
+  closeMatch: {
+    fontSize: 16,
+    color: 'rgba(255, 255, 255, 0.8)',
+    textAlign: 'center',
+    marginTop: 30,
+    fontWeight: 'bold',
   },
 });
